88a1d557b5447c499f0a2761e7743f60b85ebf26
Nuri Akseli <nuriakseli@outlook.com>
2021-09-21 21:18:53 +0300

    Dependancy Resolver Added

---
+++ Blog/Blog.Business/DependancyResolvers/Autofac/BusinessModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Blog.Business.Abstract;
+using Blog.Business.Concrete;
+using Blog.DataAccess.Abstract;
+using Blog.DataAccess.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Business.DependancyResolvers.Autofac
+{
+    public class BusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AboutManager>().As<IAboutService>().SingleInstance();
+            builder.RegisterType<ArticleManager>().As<IArticleService>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<CommentManager>().As<ICommentService>().SingleInstance();
+            builder.RegisterType<ContactManager>().As<IContactDal>().SingleInstance();
+            builder.RegisterType<WriterManager>().As<IWriterService>().SingleInstance();
+
+            builder.RegisterType<EfAboutDal>().As<IAboutDal>();
+            builder.RegisterType<EfArticleDal>().As<IArticleDal>();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
+            builder.RegisterType<EfCommentDal>().As<ICommentDal>();
+            builder.RegisterType<EfContactDal>().As<IContactDal>();
+            builder.RegisterType<EfWriterDal>().As<IWriterDal>();
+
+
+            builder.RegisterType<BlogContext>().As<DbContext>();
+        }
+        
+    }
+}
---
+++ Blog/Blog.Business/DependancyResolvers/Autofac/InstanceFactory.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Business.DependancyResolvers.Autofac
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<BusinessModule>();
+            var container = builder.Build();
+            return container.Resolve<T>();
+        }
+    }
+}
